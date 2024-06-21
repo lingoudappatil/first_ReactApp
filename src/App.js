436adce74d728de2552de861b2bca6ddc99e397e
lingoudappatil <136546601+lingoudappatil@users.noreply.github.com>
2024-06-21 21:47:15 +0530

    comp

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 
 import './App.css';
 import Hello from './components/Hello';
+// import Button from './components/Comon';
 import Message from './components/Message';
+import Msg from './components/Msg';
 
 function App() {
   return (
     <div className="App">
       <Hello/>
       <Message />
+      <Msg />
     </div>
   );
 }
---
+++ src/components/Msg.js
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+class Msg  extends Component
+{
+    render()
+    {
+       return  <h1>Lingoudab Patil</h1>
+    }
+}
+
+export default Msg;
