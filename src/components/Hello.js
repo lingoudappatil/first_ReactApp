b4d1eb2f6de72ef7b513bdd90872eab348ddcbb4
lingoudappatil <136546601+lingoudappatil@users.noreply.github.com>
2024-06-25 21:44:21 +0530

    Function Component

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,4 +1,11 @@
-const hello =() =>
-     <h1>Hello World From Function components</h1>
+console.log("First line execut");
+console.log("Second line execute");
+function hello()
+{
+    console.log("This wiil execute after some time");
+}
+setTimeout(hello,3000);
+console.log("Third line execute");
+console.log("Forth line execute");
 
 export default hello;
