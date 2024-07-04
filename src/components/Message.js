30b9696a62952fff99182650dcdd95cc73d84a8b
Lingouda Patil <136546601+lingoudappatil@users.noreply.github.com>
2024-07-04 08:18:13 +0530

    Message.js

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,6 +1,6 @@
 import { Component }  from "react";
 class Message extends Component { 
-    render()
+render()
     {
         return <h1>This is Class Component FROM {this.props.name} Profession is {this.props.profession}</h1>
     }
