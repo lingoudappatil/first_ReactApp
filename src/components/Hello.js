246010500494779d17cda631bdc8f9a838515d14
Lingouda Patil <136546601+lingoudappatil@users.noreply.github.com>
2024-07-06 23:01:30 +0530

    Hello.js

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -3,7 +3,7 @@ console.log("Second line execute");
 function hello()
 {
     console.log("This wiil execute after some time");
-}
+} 
 setTimeout(hello,3000); 
 console.log("Third line execute");
 console.log("Forth line execute");
