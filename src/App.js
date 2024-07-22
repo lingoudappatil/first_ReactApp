d1820cec4b058df4231499a2e5753da6bb378cca
Lingouda Patil <136546601+lingoudappatil@users.noreply.github.com>
2024-07-22 22:33:51 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Hello from './components/Hello'; 
 import Message from './components/Message';
 import Msg from './components/Msg';
-import Msg2 from './components/Msg2';
+import Msg2 from './components/Msg2'; 
 import Button from './components/Button';
   
 function App() { 
